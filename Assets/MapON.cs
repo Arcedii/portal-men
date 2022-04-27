7bbeba7f5a9f60cc9297b58268a54e973c3d9edd
Arcedii <91275440+Arcedii@users.noreply.github.com>
2022-04-27 17:34:32 +0300

    impressive changes
    added trees and bushes,camp fire,map,camping location.

---
+++ Assets/MapON.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapON : MonoBehaviour
+{
+   public Button on;
+   public Image Map;
+    public Button of;
+
+    void Start()
+    {
+        of.onClick.AddListener(Of);
+        on.onClick.AddListener(On);
+        Map.gameObject.SetActive(false);
+        of.gameObject.SetActive(false);
+    }
+
+
+   
+    
+    void On()
+    {
+        of.gameObject.SetActive(true);
+        Map.gameObject.SetActive(true);
+        on.gameObject.SetActive(false);
+    }
+
+    void Of()
+    {
+        of.gameObject.SetActive(false);
+        Map.gameObject.SetActive(false);
+        on.gameObject.SetActive(true);
+    }
+}
